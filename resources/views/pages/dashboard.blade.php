41ca17f55927e299fa74924dbda41feec46d113a
Muhammad Sholeh <muhammadsholeh.dev@gmail.com>
2023-01-24 22:31:06 +0700

    feat: 🎸 Dashboard
    add boilerplate

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('title', 'Dashboard')
+
+@section('css')
+{{-- ADDITIONAL CSS GOES HERE --}}
+
+
+{{-- ADDITIONAL CSS GOES HERE --}}
+@endsection
+
+@section('content')
+{{-- CONTENT GOES HERE --}}
+
+{{-- CONTENT GOES HERE --}}
+@endsection
