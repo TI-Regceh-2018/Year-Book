1aa2fe30d1e976daddde215877791ddccea9c5b4
Muhammad Sholeh <muhammadsholeh.dev@gmail.com>
2023-01-25 09:33:46 +0700

    feat: 🎸 Navbar
    slicing navbar from figma design

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light top-navbar" data-toggle="sticky-onscroll">
+    <nav class="navbar navbar-expand-lg navbar-light top-navbar px-3" data-toggle="sticky-onscroll">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand text-uppercase nav-brand" href="#">REGCEH 18</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -10,27 +10,20 @@
 
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav pull-right">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Home</a>
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark p-0" href="#">Love</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Services</a>
+                        <a class="nav-link text-dark p-0" href="#">Cerita
+                            Kocak</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Our Work</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                        <a class="nav-link text-dark p-0" href="#">Album</a>
                     </li>
                 </ul>
             </div>
         </div>
+        </div>
     </nav>
-
+    <div class="underline">
 </header>
